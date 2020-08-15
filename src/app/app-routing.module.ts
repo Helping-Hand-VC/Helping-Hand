091f60723c0f92466353346bd14f85ce905b93f9
Kai-Davey <18003675@vcconnect.co.za>
2020-08-15 22:56:16 +0200

    First Upload Of All Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Login', component: LoginComponent },
+  { path: 'Register', component: RegisterComponent },
+
+  { path: '**', component:  LoginComponent},//Replace with 404 Page maybe?
+  { path: '', component:  LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
