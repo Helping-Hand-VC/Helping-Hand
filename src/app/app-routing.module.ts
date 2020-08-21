c0d989f56f014d0ae7c0049f505961adc6c0d76b
DESKTOP-UPPD7A0\Jon Pallatte <mitchelljonpallatte@gmail.com>
2020-08-21 13:25:03 +0200

    Accordion Added to profile page - Jon Pallatte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { ProfilePageComponent } from './Components/profile-page/profile-page.component';
 
 
 const routes: Routes = [
   { path: 'Login', component: LoginComponent },
   { path: 'Register', component: RegisterComponent },
+  { path: 'profile', component: ProfilePageComponent },
 
   { path: '**', component:  LoginComponent},//Replace with 404 Page maybe?
   { path: '', component:  LoginComponent}
