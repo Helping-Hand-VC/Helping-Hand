0ca2077ad3f182c6ee439c9a80c523ea05ceb023
peninaberkowitz@gmail.com <peninaberkowitz@gmail.com>
2020-08-21 14:09:04 +0200

    Login and Register Backend

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 //For Firebase
-import {FirebaseConnectionService} from 'src/app/Firebase/firebase-connection.service';
-import {Student} from 'src/app/models/Users/user.model';
+import { FirebaseConnectionService } from 'src/app/Firebase/firebase-connection.service';
+import { Student } from 'src/app/models/Users/user.model';
+
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,44 @@ import {Student} from 'src/app/models/Users/user.model';
 })
 export class LoginComponent implements OnInit {
 
+  
+  blnValid: boolean = true;
   clsStudent: Student;
   constructor(private clsFirebaseConnectionService: FirebaseConnectionService) { }
 
   ngOnInit(): void {
+
+  }
+
+  async SignInUser() {
+    var username = (<HTMLInputElement>document.getElementById("username")).value;
+    var psw = (<HTMLInputElement>document.getElementById("psw")).value;
+    this.ValidateUserInfo(username, psw);
+    if (this.blnValid)
+      console.log(await this.clsFirebaseConnectionService.SignInUser(username,psw));
+    //"test@gmaisl.com", "123123"
+  }
+
+  ValidateUserInfo(uemail: string, pwd: string): void {
+    let regexPass = new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$');
+    let regexEmail = new RegExp('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$');
+    if ((uemail == "") || (pwd == "")) {
+      this.blnValid = false;
+      //display message - insert username
+      window.alert("Please can you insert your username and password.");
+
+    }
+    
+    if (!regexPass.test(pwd)) {
+      this.blnValid = false;
+      window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, and is at least 6 characters long");
+    }
+    
+
+    if (!regexEmail.test(uemail)) {
+      this.blnValid = false;
+      window.alert("Please insert a vaild email address");
+    }
 
   }
 
--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,15 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 
+//firebase imports
+import { FirebaseConnectionService } from 'src/app/Firebase/firebase-connection.service';
+import { Student } from 'src/app/models/Users/user.model'
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  //email: string;
+  //psw: string;
+  blnValid: boolean = true;
+  clsStudent: Student;
+  constructor(private clsFirebaseConnectionService: FirebaseConnectionService) { }
 
   ngOnInit(): void {
   }
+  
+  async RegisterUser() {
+    var email = (<HTMLInputElement>document.getElementById("userEmail")).value;
+    var psw = (<HTMLInputElement>document.getElementById("psw")).value;
+    var cnf = (<HTMLInputElement>document.getElementById("cnfpsw")).value;
+    var utype = (<HTMLInputElement>document.getElementById("utype")).value;;
+    this.ValidateUserInfo(email, utype, psw, cnf);
+    if (this.blnValid)
+      console.log("Worked")
+      console.log(await this.clsFirebaseConnectionService.CreateNewUser(email, psw, utype));
+    //"test@gmaisl.com", "123123"
+  }
+
+  ValidateUserInfo(uemail: string, utype: string,pwd: string, cnfps: string): void {
+    this.blnValid = true;
+    let regexHMPass = new RegExp('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$');
+    let regexTeacherPass = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Zaz\\d@$!%*?&]{10,20}');
+    let regexParentPass = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Zaz\\d@$!%*?&]{8,20}');
+    let regexStudPass = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$');
+    let regexEmail = new RegExp('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$');
+    /* 
+    let regexId = new RegExp('^(\d{13})?$');
+    let regexPhone = new RegExp('^[0-9]{10}$');
+    let regexFName = new RegExp('^[a-zA-Z\' -]+$');
+    let regexLName = new RegExp('^[a-zA-Z\' -]+$');
+    */
+
+    
+    if ((uemail == "") || (pwd == "")) {
+      this.blnValid = false;
+      //display message - insert username
+      window.alert("Please can you insert your email and password.");
+    }
+    if(pwd != cnfps){
+      this.blnValid = false;
+      window.alert("Passwords do not match.");
+    }
+
+    //Password Validation for specific user types
+    if (utype == "") {
+      this.blnValid = false;
+      window.alert("Please select a Role.");
+    } else if (utype == "headmaster") {
+       
+      if (!regexHMPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your passwrod contains min. 2 numbers, 2 capitals, 2 special characters, and is at least 13 characters long");
+        
+      }
+    } else if (utype == "teacher") {
+      if (!regexTeacherPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, a special characters, and is at least 10 characters long");
+      }
+    } else if (utype == "parent") { 
+      if (!regexParentPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, a special characters, and is at least 8 characters long");
+      }
+    } else if (utype == "student") {
+      if (!regexStudPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, and is at least 6 characters long");
+      }
+    }
+    
+    
+    //Validate Email
+    if (!regexEmail.test(uemail)) {
+      this.blnValid = false;
+      window.alert("Please insert a valid email address");
+    }
+
+
+  }
+  
 
 }
