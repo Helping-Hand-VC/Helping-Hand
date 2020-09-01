d4b04450ffeca6e066e3cee5f2a913c5cbb66e7e
peninaberkowitz@gmail.com <peninaberkowitz@gmail.com>
2020-09-01 22:20:55 +0200

    Updated Register Backend

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -14,30 +14,63 @@ export class RegisterComponent implements OnInit {
   //psw: string;
   blnValid: boolean = true;
   clsStudent: Student;
+  userT: string;
   constructor(private clsFirebaseConnectionService: FirebaseConnectionService) { }
 
   ngOnInit(): void {
   }
-  
+
+  async studClick() {
+    this.userT = "student"
+
+    console.log(this.userT)
+  }
+  async parentClick() {
+    this.userT = "parent"
+    console.log(this.userT)
+  }
+  async teacherClick() {
+    this.userT = "teacher"
+    console.log(this.userT)
+  }
+  async hmClick() {
+    this.userT = "headmaster"
+    console.log(this.userT)
+  }
+
   async RegisterUser() {
-    var email = (<HTMLInputElement>document.getElementById("userEmail")).value;
-    var psw = (<HTMLInputElement>document.getElementById("psw")).value;
+    var email = (<HTMLInputElement>document.getElementById("RegUserEmail")).value;
+    var psw = (<HTMLInputElement>document.getElementById("RegPsw")).value;
     var cnf = (<HTMLInputElement>document.getElementById("cnfpsw")).value;
-    var utype = (<HTMLInputElement>document.getElementById("utype")).value;;
+    var utype = this.userT;
+
+
+    console.log(email, utype, psw, cnf);
     this.ValidateUserInfo(email, utype, psw, cnf);
-    if (this.blnValid)
+    if (this.blnValid) {
       console.log("Worked")
       console.log(await this.clsFirebaseConnectionService.CreateNewUser(email, psw, utype));
+      console.log(email, psw, utype)
+    } else {
+      console.log("Invalid Data");
+    }
     //"test@gmaisl.com", "123123"
   }
 
-  ValidateUserInfo(uemail: string, utype: string,pwd: string, cnfps: string): void {
+
+
+  async ValidateUserInfo(uemail: string, utype: string, pwd: string, cnfps: string) {
+
+    console.log(uemail, utype, pwd, cnfps);
     this.blnValid = true;
+
     let regexHMPass = new RegExp('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$');
     let regexTeacherPass = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Zaz\\d@$!%*?&]{10,20}');
     let regexParentPass = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Zaz\\d@$!%*?&]{8,20}');
     let regexStudPass = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$');
     let regexEmail = new RegExp('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$');
+
+
     /* 
     let regexId = new RegExp('^(\d{13})?$');
     let regexPhone = new RegExp('^[0-9]{10}$');
@@ -45,46 +78,18 @@ export class RegisterComponent implements OnInit {
     let regexLName = new RegExp('^[a-zA-Z\' -]+$');
     */
 
-    
+
+
     if ((uemail == "") || (pwd == "")) {
       this.blnValid = false;
       //display message - insert username
       window.alert("Please can you insert your email and password.");
     }
-    if(pwd != cnfps){
+    if (pwd != cnfps) {
       this.blnValid = false;
       window.alert("Passwords do not match.");
     }
 
-    //Password Validation for specific user types
-    if (utype == "") {
-      this.blnValid = false;
-      window.alert("Please select a Role.");
-    } else if (utype == "headmaster") {
-       
-      if (!regexHMPass.test(pwd)) {
-        this.blnValid = false;
-        window.alert("Password too weak. Please make sure your passwrod contains min. 2 numbers, 2 capitals, 2 special characters, and is at least 13 characters long");
-        
-      }
-    } else if (utype == "teacher") {
-      if (!regexTeacherPass.test(pwd)) {
-        this.blnValid = false;
-        window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, a special characters, and is at least 10 characters long");
-      }
-    } else if (utype == "parent") { 
-      if (!regexParentPass.test(pwd)) {
-        this.blnValid = false;
-        window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, a special characters, and is at least 8 characters long");
-      }
-    } else if (utype == "student") {
-      if (!regexStudPass.test(pwd)) {
-        this.blnValid = false;
-        window.alert("Password too weak. Please make sure your passwrod contains a number, a capital letter, and is at least 6 characters long");
-      }
-    }
-    
-    
     //Validate Email
     if (!regexEmail.test(uemail)) {
       this.blnValid = false;
@@ -92,7 +97,39 @@ export class RegisterComponent implements OnInit {
     }
 
 
-  }
-  
+    //Password Validation for specific user types
+    if (utype == "") {
+      this.blnValid = false;
+      window.alert("Please select a Role.");
+    } else if (utype == "headmaster") {
 
+      if (!regexHMPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your password contains min. 2 numbers, 2 capitals, 2 special characters, and is at least 13 characters long");
+
+      }
+    } else if (utype == "teacher") {
+      if (!regexTeacherPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your password contains a number, a capital letter, a special characters, and is at least 10 characters long");
+      }
+    } else if (utype == "parent") {
+      if (!regexParentPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your password contains a number, a capital letter, a special characters, and is at least 8 characters long");
+      }
+    } else if (utype == "student") {
+      if (!regexStudPass.test(pwd)) {
+        this.blnValid = false;
+        window.alert("Password too weak. Please make sure your password contains a number, a capital letter, and is at least 6 characters long");
+      }
+    }
+
+    var chkPolicy = (<HTMLInputElement>document.getElementById("checkpolicy"));
+    if (!chkPolicy.checked) {
+      this.blnValid = false;
+      //display message 
+      window.alert("Please accept privacy policy.");
+    }
+  }
 }
